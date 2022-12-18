ebb3aa35c250c5bfe4a945245f79e4bc27a5e160
Lela Deslauriers <lela.deslauriers@gmail.com>
2022-12-17 17:40:56 -0800

    commit tree 407b786ca2c1a657540ae530a916656aaad9246a

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+function submitData (userName, userEmail) { 
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        }),
+    })   
+    .then(function (response) {
+        return response.json();
+      })
+    .then(function(object) {
+        document.body.innerHTML = object["id"];
+    })
+    .catch(function (error) {
+        document.body.innerHTML = error["message"];
+    });
+
+
+    // .then(renderOneUser (object) {
+    //     newParagraph = document.createElement('p');
+    //     element = newParagraph.textContent = `User ID: ${object.id}`;
+    //     document.body.append(element);
+
+    //   });
+    
+    }
